3bd8af86bb18dea5f923327e75dd1e56917d2026
Zwyx <29386932+Zwyx@users.noreply.github.com>
2022-11-23 21:05:54 +0800

    Improve Giscus component

---
+++ src/theme/BlogPostItem/index.tsx
@@ -0,0 +1,26 @@
+import useIsBrowser from "@docusaurus/useIsBrowser";
+import GiscusComments from "@site/src/components/GiscusComments";
+import BlogPostItem from "@theme-original/BlogPostItem";
+import React from "react";
+
+const POST_REGEX = /^\/(blog|til)\/.+$/;
+
+export default function BlogPostItemWrapper(props) {
+	const isBrowser = useIsBrowser();
+
+	return (
+		<>
+			<BlogPostItem {...props} />
+
+			{isBrowser && window.location.pathname.match(POST_REGEX) && (
+				<>
+					<div style={{ marginTop: "32px" }} />
+
+					<GiscusComments />
+
+					<div style={{ marginTop: "-24px" }} />
+				</>
+			)}
+		</>
+	);
+}
