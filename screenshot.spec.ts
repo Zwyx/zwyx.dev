b0995b62af42ce32f059c2b9dc121bc06ca6ce37
Zwyx <29386932+Zwyx@users.noreply.github.com>
2023-11-21 18:26:45 +0800

    Set up Argos for visual regression testing before upgrading to Docusaurus v3
    https://docusaurus.io/blog/upgrading-frontend-dependencies-with-confidence-using-visual-regression-testing

---
+++ screenshot.spec.ts
@@ -0,0 +1,50 @@
+import { argosScreenshot } from "@argos-ci/playwright";
+import { test } from "@playwright/test";
+import * as cheerio from "cheerio";
+import * as fs from "fs";
+
+// Extract a list of pathnames, given a fs path to a sitemap.xml file
+// Docusaurus generates a build/sitemap.xml file for you!
+export function extractSitemapPathnames(sitemapPath: string): string[] {
+	const sitemap = fs.readFileSync(sitemapPath).toString();
+	const $ = cheerio.load(sitemap, { xmlMode: true });
+	const urls: string[] = [];
+	$("loc").each(function handleLoc() {
+		urls.push($(this).text());
+	});
+	return urls.map((url) => new URL(url).pathname);
+}
+
+// Converts a pathname to a decent screenshot name
+export function pathnameToArgosName(pathname: string): string {
+	return pathname.replace(/^\/|\/$/g, "") || "index";
+}
+
+// Constants
+const siteUrl = "http://localhost:3000";
+const sitemapPath = "./build/sitemap.xml";
+const stylesheetPath = "./screenshot.css";
+const stylesheet = fs.readFileSync(stylesheetPath).toString();
+
+// Wait for hydration, requires Docusaurus v2.4.3+
+// Docusaurus adds a <html data-has-hydrated="true"> once hydrated
+// See https://github.com/facebook/docusaurus/pull/9256
+function waitForDocusaurusHydration() {
+	return document.documentElement.dataset.hasHydrated === "true";
+}
+
+function screenshotPathname(pathname: string) {
+	test(`pathname ${pathname}`, async ({ page }) => {
+		const url = siteUrl + pathname;
+		await page.goto(url);
+		await page.waitForFunction(waitForDocusaurusHydration);
+		await page.addStyleTag({ content: stylesheet });
+		await argosScreenshot(page, pathnameToArgosName(pathname));
+	});
+}
+
+test.describe("Docusaurus site screenshots", () => {
+	const pathnames = extractSitemapPathnames(sitemapPath);
+	console.log("Pathnames to screenshot:", pathnames);
+	pathnames.forEach(screenshotPathname);
+});
