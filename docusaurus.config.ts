f611f14e2e03a9fba652b75a16e9a1b30523a65b
Zwyx <29386932+Zwyx@users.noreply.github.com>
2024-02-06 17:01:13 +0800

    Add base conversions post

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -150,11 +150,7 @@ const config: Config = {
 		},
 		footer: {
 			style: "dark",
-			copyright: `Copyright © ${new Date().getFullYear()} Zwyx.dev${
-				env.NODE_ENV !== "development"
-					? `<!-- <br /><br /> --><a style="display: none;" href="https://visitorbadge.io/status?path=zwyx.dev" target="_blank"><img src="https://api.visitorbadge.io/api/visitors?path=zwyx.dev&labelColor=%23555555&countColor=%236280a6&style=flat&labelStyle=none" /></a>`
-					: ""
-			}`,
+			copyright: `Copyright © ${new Date().getFullYear()} Zwyx.dev`,
 		},
 		prism: {
 			theme,
